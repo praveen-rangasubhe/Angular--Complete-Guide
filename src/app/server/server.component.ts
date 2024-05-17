954652b0a88597096acd061441c3b73946b354b6
praveen-rangasubhe <praveentrangasubhe@gmail.com>
2024-05-17 21:30:09 +0530

    added color dynamically

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css'],
 })
-export class serverComponent {}
+export class serverComponent {
+  /**
+   * @type {string}
+   * @memberof serverComponent
+   */
+  red: string = 'red';
+
+  /**
+   * Creates an instance of serverComponent.
+   * @memberof serverComponent
+   */
+  constructor() {
+    setTimeout(() => {
+      this.red = 'blue';
+    }, 4000);
+  }
+}
